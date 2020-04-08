72f1dea1c4943d82c052de0f65c69c3abfc28d51
Mikhail Zhadko <m.zhadko@dlg.im>
2020-04-08 10:45:20 +0300

    initial commit

---
+++ OpusIOS/OpusIOS.h
@@ -0,0 +1,27 @@
+//
+//  OpusIOS.h
+//  OpusIOS
+//
+//  Created by Marc Delling on 01.08.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpusIOS.
+FOUNDATION_EXPORT double OpusIOSVersionNumber;
+
+//! Project version string for OpusIOS.
+FOUNDATION_EXPORT const unsigned char OpusIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpusIOS/PublicHeader.h>
+
+#import <OpusIOS/opusfile.h>
+#import <OpusIOS/ogg.h>
+#import <OpusIOS/os_types.h>
+#import <OpusIOS/opus.h>
+#import <OpusIOS/opus_multistream.h>
+#import <OpusIOS/opus_custom.h>
+#import <OpusIOS/opus_types.h>
+#import <OpusIOS/opus_defines.h>
+#include<OpusIOS/opus_header.h>
---
+++ OpusIOS/OpusIOS.swift
@@ -0,0 +1,9 @@
+//
+//  OpusIOS.swift
+//  OpusIOS
+//
+//  Created by Mikhail Zhadko on 07.04.2020.
+//
+
+import Foundation
+import OpusIOS.CLibOpus
